d64b67756c8c9853742b04a5cf70ba0f2f556a87
Michael Knoppik <michael@meformobile.com>
2016-02-17 09:00:32 +0100

    Initial commit

---
+++ app/src/main/java/de/xappo/myrxjava/MainActivity.java
@@ -0,0 +1,90 @@
+package de.xappo.myrxjava;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Action1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+
+            }
+        });
+
+        Observable<String> myObservable
+                = Observable.just("Hello"); // Emits "Hello"
+
+        Action1<String> myAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                Log.d("My Action", s);
+            }
+        };
+
+        Subscription mySubscription = myObservable.subscribe(myAction);
+
+
+
+        Observable<Integer> myArrayObservable
+                = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6}); // Emits each item of the array, one at a time
+
+        myArrayObservable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer i) {
+                Log.d("My Action", String.valueOf(i)); // Prints the number received
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
