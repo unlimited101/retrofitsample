2069b2c5bf852e5f9c829a0df066068f9a7efe58
Michael Knoppik <Michael.Knoppik@mk-xappo.de>
2016-04-12 11:21:21 +0200

    Change minSdk to 16 Timber support added Date creation added

---
+++ app/src/main/java/de/xappo/myrxjava/MyRxJavaApplication.java
@@ -0,0 +1,45 @@
+package de.xappo.myrxjava;
+
+import android.app.Application;
+import android.util.Log;
+
+import timber.log.Timber;
+
+import static timber.log.Timber.DebugTree;
+
+/**
+ * Created by knoppik on 12.04.16.
+ */
+public class MyRxJavaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    /** A tree which logs important information for crash reporting. */
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            FakeCrashLibrary.log(priority, tag, message);
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    FakeCrashLibrary.logError(t);
+                } else if (priority == Log.WARN) {
+                    FakeCrashLibrary.logWarning(t);
+                }
+            }
+        }
+    }
+}
+
--- app/src/main/java/de/xappo/myrxjava/rxjava/RxJavaEmitSequentiallyActivity.java
+++ app/src/main/java/de/xappo/myrxjava/rxjava/RxJavaEmitSequentiallyActivity.java
@@ -1,22 +1,33 @@
 package de.xappo.myrxjava.rxjava;
 
 import android.annotation.TargetApi;
-import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.xappo.myrxjava.R;
+import timber.log.Timber;
 
 public class RxJavaEmitSequentiallyActivity extends AppCompatActivity {
+
+    Date mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_emit_sequentially);
         ButterKnife.bind(this);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.btn_create_date)
+    public void buttonCreateDateClick() {
+        mDate = new Date();
+        Timber.i("Date is: %s", mDate.toString());
     }
 
     @SuppressWarnings("unused")
